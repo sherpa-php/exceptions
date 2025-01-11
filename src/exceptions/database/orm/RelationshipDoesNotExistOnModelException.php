a4bfe3754dafa23d8c0bb7dfcd7db3e19f728042
belicfr <96206878+belicfr@users.noreply.github.com>
2025-01-10 21:52:02 -0500

    feat: create RelationshipDoesNotExistOnModelException

---
+++ src/exceptions/database/orm/RelationshipDoesNotExistOnModelException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sherpa\Core\exceptions\database;
+
+use Sherpa\Core\exceptions\SherpaException;
+use Throwable;
+
+/**
+ * Sherpa Exception
+ * <p>
+ *     To throw if given relationship method
+ *     does no longer exist on given model class.
+ * </p>
+ */
+class RelationshipDoesNotExistOnModelException extends SherpaException
+{
+    public function __construct(string $relationship,
+                                string $model,
+                                ?Throwable $previous = null)
+    {
+        $message = "$relationship() relationship does no longer exist on $model model.";
+
+        parent::__construct($message, 1252, $previous);
+    }
+}
